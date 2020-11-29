14b38b5bd6c175cc7f06e9de4734bf5667967cdd
Aldrich Suratos <cyurio@gmail.com>
2020-11-30 08:47:25 +0900

    cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -21,9 +21,9 @@ impl State {
 }
 
 impl GameState for State {
-    fn tick(&mut self, ctx : &mut Rltk) {
+    fn tick(&mut self, ctx: &mut Rltk) {
         ctx.cls();
-        
+
         self.run_systems();
 
         player_input(self, ctx);
@@ -33,23 +33,19 @@ impl GameState for State {
 
         let positions = self.ecs.read_storage::<Position>();
         let renderables = self.ecs.read_storage::<Renderable>();
-        
+
         for (pos, render) in (&positions, &renderables).join() {
             ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph);
         }
     }
 }
 
-
-
 fn main() -> rltk::BError {
     use rltk::RltkBuilder;
     let context = RltkBuilder::simple80x50()
         .with_title("Roguelike Tutorial")
         .build()?;
-    let mut gs = State{ 
-        ecs: World::new()
-    };
+    let mut gs = State { ecs: World::new() };
 
     gs.ecs.register::<Position>();
     gs.ecs.register::<Renderable>();
@@ -61,15 +57,18 @@ fn main() -> rltk::BError {
     let (player_x, player_y) = rooms[0].center();
 
     gs.ecs
-      .create_entity()
-      .with(Position { x: player_x, y: player_y })
-      .with(Renderable {
-          glyph: rltk::to_cp437('@'),
-          fg: RGB::named(rltk::YELLOW),
-          bg: RGB::named(rltk::BLACK), 
-       })
-      .with(Player {})
-      .build();
-    
+        .create_entity()
+        .with(Position {
+            x: player_x,
+            y: player_y,
+        })
+        .with(Renderable {
+            glyph: rltk::to_cp437('@'),
+            fg: RGB::named(rltk::YELLOW),
+            bg: RGB::named(rltk::BLACK),
+        })
+        .with(Player {})
+        .build();
+
     rltk::main_loop(context, gs)
 }
